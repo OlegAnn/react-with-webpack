b8e60daf210266bb064347f9588377eb05052a22
OlegAnn <olegihimich@gmail.com>
2018-05-17 15:57:47 +0300

    second commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-console.log('hello world');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Hello = ({name}) => `Hello, ${name}`;
+
+ReactDom.render(
+    <Hello name="Kharkiv" />,
+    document.getElementById('root')
+);
